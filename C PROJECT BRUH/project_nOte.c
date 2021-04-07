15c864fecd3596ecd8ba3589fc7cfb93f5a4f7c7
Ayman Lyesri <lyesri99@gmail.com>
2021-04-07 15:50:54 +0100

    Project nOte ENHANCED

--- C PROJECT BRUH/project_nOte.c
+++ C PROJECT BRUH/project_nOte.c
@@ -1,53 +1,73 @@
+/*PROGRAMME THAT WILL ENSURE THE FINAL RESULT WITHOUT MUCH CALCULATIONS !!!
+PROGRAMMED AND DESIGNED BY THE ONE AND ONLY AYMAN LYESRI...*/
+
 #include <stdio.h>
 main()
 {
 	printf("Welcome!!\n");
 	printf("This programe will show you how much you will have for each Module\n\n\n\n");
-	float tab[3][2],x=0,y=0,h=0,result=0,bonus;
+	float tab[9][9],x=0,y=0,h=0,result=0,bonus;
 	int i=0,j=0;
 	printf("first of all... enter the note that you think it representes\n the note of the absence as well as the participation-------->");
 	scanf("%f",&bonus);
 	printf("\n\n\n\n");
-	bonus=bonus*0.2;
-	while(i<4){
-		while(j<2){
-			if(j==0&&i==0){
-			printf("  #Please enter the notes (#0-20#) of Module-->*ALGEBRA*<--//Test_numero->1<-------:");
-			scanf("%f",&tab[i][j]);
-			}
-			else if(j==0&&i==1){
-			printf("  #Please enter the notes (#0-20#) of Module-->*HTML*<--//Test_numero---->1<-------:");
-			scanf("%f",&tab[i][j]);
-			}
-			else if(j==0&&i==2){
-			printf("  #Please enter the notes (#0-20#) of Module-->*C++*<--//Test_numero----->1<-------:");
-			scanf("%f",&tab[i][j]);
-			}
-			else if(j==0&&i==3){
-			printf("  #Please enter the notes (#0-20#) of Module-->*English*<--//Test_numero----->1<-------:");
-			scanf("%f",&tab[i][j]);
-			}
-			else if(j>0){
-			printf("\tExam_numero --------------------->1<---------------------------:");
-			scanf("%f",&tab[i][j]);
+	bonus=bonus*0.2;                                                    //bonus regulations...
+	while(i<8){
+		while(j<1){
+			switch(i){
+				case 0:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*ALGEBRA*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+			    case 1:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*ANALITIQ*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 2:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*FRENCH*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 3:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*ENGLISH*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 4:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*C++*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 5:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*LINUX*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 6:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*WINDOWS ARCH*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
+				case 7:
+			    printf("  #Please enter the notes (#0-20#) of Module-->*JAVASCRIPT*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);break;
 			}
 			j++;
+			printf("\tExam_numero --------------------->1<---------------------------:");
+			scanf("%f",&tab[i][j]);
 		}
 		i++;
 		j=0;
-		printf("\n");
+		printf("\n\n");
 	}                                                                         /* i : for the module--------------------------*/
 	i=0,j=0;                    
-	while(i<3){                                                               /* j : if its a test or an exam----------------*/
-		if(i==0){
-			printf(" *ALGEBRA*-->\t");
-		}
-		if(i==1){
-			printf(" *HTML*----->\t");
-		}
-		if(i==2){
-			printf(" *C++*------>\t");
-		}
+	while(i<8){                                                               /* j : if its a test or an exam----------------*/
+		switch(i){
+			case 0:
+			printf(" *ALGEBRA*----------->\t");  break;
+		    case 1:
+			printf(" *ANALITIQ*---------->\t");  break;
+		    case 2:
+			printf(" *FRENCH*------------>\t");  break;
+			case 3:
+			printf(" *ENGLISH*----------->\t");  break;
+			case 4:
+			printf(" *C++*--------------->\t");  break;
+			case 5:
+			printf(" *LINUX*------------->\t"); break; 
+			case 6:
+			printf(" *WINDOWS ARCH*------>\t"); break; 
+			case 7:
+			printf(" *JAVASCRIPT*-------->\t");  break;  
+		}                                                  
 		while(j<2){
 			printf(" %f \t",tab[i][j]);
 			if(j==0){
@@ -59,20 +79,23 @@ main()
 			j++;
 		}                                                                     /* --result : the result of a specific module--*/
 		x=x*0.3,h=h*0.5;
-		result=x+h+bonus;                                                     /* bonus : bonus points for participation etc..*/
+		result=x+h+bonus;                                                     /* bonus (*0.2) : bonus points for participation etc..*/
+
 		printf("*RESULT*--> %f <-- \n",result);
 		y=y+result;
 		x=0,h=0,j=0,result=0;
 		i++;
 	}
-	y=y/3;                                                                    /* ---------------y : final result-------------*/
+	y=y/8;                                                                    /* ---------------y : final result-------------*/
 	printf("\n\n\t\t\t The final result is:\n\n\t\t\t----[[ %f ]]----\n",y);
 	if(y>10){
-		printf("\n\t>---You'll pass!!---<");
+		printf("\n\n\t\t\t>---You'll pass!!---<");
 	}
 	else{
 		printf("\n\t>---You need to take another exam for Evaluation!!---<");
 	}
-	printf("\n\t\t\t\t\t\t\t\t\t\t\t Production by ayman lyesri :)");
+	printf("\n\t\t\t\t\t\t\t\t Production by ayman lyesri :)\n");
+	system ("pause");
+	system ("pause");
 	return 0;
 }
