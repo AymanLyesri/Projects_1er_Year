7fce5a7cdf2b0557a568bad5cead952ebc3e0dca
Ayman Lyesri <lyesri99@gmail.com>
2021-04-09 16:28:29 +0100

    project_NOTE optimized (saving feature added)

--- C PROJECT BRUH/project_nOte.c
+++ C PROJECT BRUH/project_nOte.c
@@ -1,75 +1,93 @@
 /*PROGRAMME THAT WILL ENSURE THE FINAL RESULT WITHOUT MUCH CALCULATIONS !!!
 PROGRAMMED AND DESIGNED BY THE ONE AND ONLY AYMAN LYESRI...*/
 
+#include <stdlib.h>
 #include <stdio.h>
-main()
-{
+
+
+void storage(float val[][2], int bonus, float result){                     //to store the values create a file and name it : "bruh.txt"
+	int tab[16];                                              
+    int i=0,j=0,x=0;
+	
+    while(i<9){
+        while(j<2){
+            tab[x]=val[i][j];
+            j++,x++;
+        }
+    i++,j=0;
+    }
+    i=0,j=0,x=0;
+	FILE *f;
+	f = fopen("bruh.txt","w+");
+	fseek(f,0,SEEK_END);
+    while(i<16){
+		fprintf(f," %d ",tab[i]);
+		i++;
+    }
+	fprintf(f,"   bonus : %d  // result : %f ",bonus,result);
+}
+ //                                                             ---the values will be erased once the programme is being used again...
+void modules(int x,int y){                        //function for displaying different modules...
+    char nom[25];
+	if(y==0){
+			system("cls");
+	}
+    switch(x){
+        case 0:
+    strcpy(nom," ALGEBRA               ");
+    printf("%s",nom);break;
+        case 1:
+    strcpy(nom," ANALITIQ              ");
+    printf("%s",nom);break;
+        case 2:
+    strcpy(nom," FRENCH                ");
+    printf("%s",nom);break;
+        case 3:
+    strcpy(nom," ENGLISH               ");
+    printf("%s",nom);break;
+        case 4: 
+    strcpy(nom," C++                   ");
+    printf("%s",nom);break;
+        case 5:
+    strcpy(nom," LINUX - git           ");
+    printf("%s",nom);break;
+        case 6:
+    strcpy(nom," WINDOWS - network     ");
+    printf("%s",nom);break;
+        case 7:
+    strcpy(nom," JAVASCRIPT - economics");
+    printf("%s",nom);break;
+    }
+
+}
+
+
+void main(){
 	printf("Welcome!!\n");
 	printf("This programe will show you how much you will have for each Module\n\n\n\n");
-	float tab[9][9],x=0,y=0,h=0,result=0,bonus;
+	float tab[8][2],x=0,y=0,h=0,result=0,bonus,bp;
 	int i=0,j=0;
 	printf("first of all... enter the note that you think it representes\n the note of the absence as well as the participation-------->");
-	scanf("%f",&bonus);
+	scanf("%f",&bp);
 	printf("\n\n\n\n");
-	bonus=bonus*0.2;                                                    //bonus regulations...
+	bonus=bp;                                                                 //bp : bonus point
+	bonus=bonus*0.2;                                                          //bonus = bonus point regulations...
 	while(i<8){
 		while(j<1){
-			switch(i){
-				case 0:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*ALGEBRA*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-			    case 1:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*ANALITIQ*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 2:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*FRENCH*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 3:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*ENGLISH*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 4:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*C++*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 5:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*LINUX*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 6:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*WINDOWS ARCH*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-				case 7:
-			    printf("  #Please enter the notes (#0-20#) of Module-->*JAVASCRIPT*<--//Test_numero->1<-------:");
-			    scanf("%f",&tab[i][j]);break;
-			}
+			    modules(i,0);printf("*<--//Test_numero->1<-------:");
+			    scanf("%f",&tab[i][j]);
 			j++;
-			printf("\tExam_numero --------------------->1<---------------------------:");
+			printf("\n\tExam_numero --------------------->1<---------------------------:");
 			scanf("%f",&tab[i][j]);
 		}
-		i++;
-		j=0;
+		i++,j=0;
 		printf("\n\n");
 	}                                                                         /* i : for the module--------------------------*/
 	i=0,j=0;                    
 	while(i<8){                                                               /* j : if its a test or an exam----------------*/
-		switch(i){
-			case 0:
-			printf(" *ALGEBRA*----------->\t");  break;
-		    case 1:
-			printf(" *ANALITIQ*---------->\t");  break;
-		    case 2:
-			printf(" *FRENCH*------------>\t");  break;
-			case 3:
-			printf(" *ENGLISH*----------->\t");  break;
-			case 4:
-			printf(" *C++*--------------->\t");  break;
-			case 5:
-			printf(" *LINUX*------------->\t"); break; 
-			case 6:
-			printf(" *WINDOWS ARCH*------>\t"); break; 
-			case 7:
-			printf(" *JAVASCRIPT*-------->\t");  break;  
-		}                                                  
+	printf("MODULE---------> ");modules(i,1);printf(" -->");                                                
 		while(j<2){
-			printf(" %f \t",tab[i][j]);
+			printf("\t %f \t",tab[i][j]);
 			if(j==0){
 				x=x+tab[i][j];                                                /* --------------x : for the tests-------------*/
 			}
@@ -83,7 +101,7 @@ main()
 
 		printf("*RESULT*--> %f <-- \n",result);
 		y=y+result;
-		x=0,h=0,j=0,result=0;
+		x=0,h=0,j=0;
 		i++;
 	}
 	y=y/8;                                                                    /* ---------------y : final result-------------*/
@@ -94,7 +112,10 @@ main()
 	else{
 		printf("\n\t>---You need to take another exam for Evaluation!!---<");
 	}
-	printf("\n\t\t\t\t\t\t\t\t Production by ayman lyesri :)\n");
+	printf("\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t Production by ayman lyesri :)\n");
+
+	storage(tab,bp,y);
+
 	system ("pause");
 	system ("pause");
 	return 0;
